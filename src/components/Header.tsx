fb6a8e664a976befd780e09232e6c670bcc1dd4a
MUSTAFA ALIN <mstf_alin@hotmail.com>
2026-02-15 17:33:55 +0300

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -109,7 +109,7 @@ export function Header({
 
             <div className="flex flex-col items-end gap-1">
               <div className="flex items-center gap-1.5 bg-white/12 px-2 py-1 rounded-lg border border-white/20 min-w-[76px] justify-center">
-  <img src={ICON_BASE_URL + '/gems.png'} alt="Gems" className="w-4 h-4" />
+  <img src={ICON_BASE_URL + '/gem.png'} alt="Gems" className="w-4 h-4" />
   <span className="text-sm font-black leading-none">{gems}</span>
 </div>
 
