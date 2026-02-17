64cb442750502feebf83d92b250942063e41bcb2
MUSTAFA ALIN <mstf_alin@hotmail.com>
2026-02-17 04:29:40 +0300

    Updated CharacterDisplay.tsx

--- src/components/CharacterDisplay.tsx
+++ src/components/CharacterDisplay.tsx
@@ -11,9 +11,13 @@ export function CharacterDisplay({
   carImage,
 }: CharacterDisplayProps) {
   return (
-<div className="relative w-full h-full flex items-end justify-start overflow-hidden pb-4 pl-8">
-  {carImage && (
-        <div className="absolute bottom-0 left-4 z-5 select-none pointer-events-none" style={{ transform: 'translateX(-10px)' }}>
+    <div className="relative w-full h-full overflow-hidden">
+      {/* CAR (left side, fixed) */}
+      {carImage && (
+        <div
+          className="absolute bottom-0 left-4 z-5 select-none pointer-events-none"
+          style={{ transform: 'translateX(-10px)' }}
+        >
           <div className="w-[420px] h-[280px] sm:w-[480px] sm:h-[320px] opacity-95">
             <img
               src={carImage}
@@ -24,18 +28,20 @@ export function CharacterDisplay({
           </div>
         </div>
       )}
-      <div className="relative z-10 flex flex-col items-center">
-  <div className="relative select-none transform origin-bottom scale-75 translate-x-2 translate-y-8">
-    <div className="relative w-72 h-[500px] sm:w-80 sm:h-[550px] overflow-hidden">
-      <img
-        src={characterImage}
-        alt={characterName}
-        className="w-full h-full object-contain"
-        draggable={false}
-      />
-    </div>
-  </div>
-</div>
+
+      {/* CHARACTER (right 1/3 column, scaled down, moved down) */}
+      <div className="absolute bottom-0 right-0 z-10 w-1/3 flex justify-end pr-2 pointer-events-none">
+        <div className="select-none transform origin-bottom scale-75 translate-y-10">
+          <div className="relative w-72 h-[500px] sm:w-80 sm:h-[550px] overflow-hidden">
+            <img
+              src={characterImage}
+              alt={characterName}
+              className="w-full h-full object-contain"
+              draggable={false}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
