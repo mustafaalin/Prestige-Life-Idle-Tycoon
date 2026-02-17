81b3bfc23e6bd86c50472d0f6bfdd97b25803a0b
MUSTAFA ALIN <mstf_alin@hotmail.com>
2026-02-17 13:17:32 +0300

    Updated CharacterDisplay.tsx

--- src/components/CharacterDisplay.tsx
+++ src/components/CharacterDisplay.tsx
@@ -32,7 +32,7 @@ export function CharacterDisplay({
       )}
 
       {/* CHARACTER (right 1/3 column) */}
-      <div className="absolute bottom-8 right-0 z-20 w-1/3 flex justify-end pr-2">
+      <div className="absolute bottom-12 right-0 z-20 w-1/3 flex justify-end pr-2">
         <div className="select-none transform origin-bottom scale-75 translate-x-6">
           <div className="relative w-72 h-[500px] sm:w-80 sm:h-[550px]">
             <img
