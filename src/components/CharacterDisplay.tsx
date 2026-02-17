406e44c9f3bc68c093976886398255a6df552747
MUSTAFA ALIN <mstf_alin@hotmail.com>
2026-02-17 04:31:26 +0300

    Updated CharacterDisplay.tsx

--- src/components/CharacterDisplay.tsx
+++ src/components/CharacterDisplay.tsx
@@ -9,13 +9,14 @@ export function CharacterDisplay({
   characterImage,
   characterName,
   carImage,
+  onClickCharacter,
 }: CharacterDisplayProps) {
   return (
-    <div className="relative w-full h-full overflow-hidden">
-      {/* CAR (left side, fixed) */}
+    <div className="relative w-full h-full min-h-[560px] overflow-hidden">
+      {/* CAR (left side) */}
       {carImage && (
         <div
-          className="absolute bottom-0 left-4 z-5 select-none pointer-events-none"
+          className="absolute bottom-0 left-4 z-10 select-none pointer-events-none"
           style={{ transform: 'translateX(-10px)' }}
         >
           <div className="w-[420px] h-[280px] sm:w-[480px] sm:h-[320px] opacity-95">
@@ -29,15 +30,17 @@ export function CharacterDisplay({
         </div>
       )}
 
-      {/* CHARACTER (right 1/3 column, scaled down, moved down) */}
-      <div className="absolute bottom-0 right-0 z-10 w-1/3 flex justify-end pr-2 pointer-events-none">
-        <div className="select-none transform origin-bottom scale-75 translate-y-10">
-          <div className="relative w-72 h-[500px] sm:w-80 sm:h-[550px] overflow-hidden">
+      {/* CHARACTER (right 1/3 column) */}
+      <div className="absolute bottom-0 right-0 z-20 w-1/3 flex justify-end pr-2">
+        {/* scale + position */}
+        <div className="select-none transform origin-bottom scale-75 translate-x-6 translate-y-6">
+          <div className="relative w-72 h-[500px] sm:w-80 sm:h-[550px]">
             <img
               src={characterImage}
               alt={characterName}
               className="w-full h-full object-contain"
               draggable={false}
+              onClick={() => onClickCharacter?.()}
             />
           </div>
         </div>
